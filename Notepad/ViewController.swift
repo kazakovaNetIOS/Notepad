636b7e86b7983b3d6812eb1523b60e4bc774e635
nkazakova <natalia.kazakova@innoway.ru>
2019-06-07 23:43:54 +0300

    Initial commit

---
+++ Notepad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notepad
+//
+//  Created by Natalia Kazakova on 07/06/2019.
+//  Copyright © 2019 Natalia Kazakova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
